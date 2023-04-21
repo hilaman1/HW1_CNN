e27bd4ab9542192118554084ee5d66af9d07e37d
hilaman1 <hilaman1@mail.tau.ac.il>
2023-04-21 23:04:46 +0300

    finished part 3

--- hw1/answers.py
+++ hw1/answers.py
@@ -9,7 +9,7 @@ math (delimited with $$).
 # Part 2 answers
 
 part2_q1 = r"""
-**Your answer:**
+'''
 For k = 1 we label an image based only on the closest image to it in the training set. In many cases this will suffice,
 but in some cases relying on just 1 image is not good enough, and so increasing k up to k = 3 improves generalization according 
 to the results we got. Statistically, a relatively small amount of closest images generalizes better than just 1 image.
@@ -18,6 +18,7 @@ If k is increased beyond that, the bigger k is, the more images that do not belo
 labeling process of the image, and thus interfere with it, which causes the accuracy of the prediction to drop. 
 We can see this explicitly in the results we obtained, and so we conclude that increasing k further 
 (further than k = 3 in this case) will lead to worse generalization for unseen data.
+'''
 """
 # ==============
 
@@ -25,41 +26,51 @@ We can see this explicitly in the results we obtained, and so we conclude that i
 # Part 3 answers
 
 part3_q1 = r"""
-**Your answer:**
+'''
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+The selection of $\Delta>0$ is arbitrary for the SVM loss because we treat errors based on the $\lambda$
+ parameter. The errors won't be so bad if it's very small. We will concentrate more on minimizing $||w||$.
+  Otherwise, each error has a greater weight.
+   There is a trade-off between the data loss and the regularization loss.
+    The role of $\Delta$ is similar but reversed - the smaller $\Delta$ we can violate more,
+     and the factor of $||w||$ More meaning, which is like a big regularization.
+'''
 """
 
 part3_q2 = r"""
-**Your answer:**
+'''
+1. In the sample image, the yellow area corresponds with the white area and the blue
+ and green areas correspond with the black area, so the class gives a higher score.
+  We can see that the yellow lines represent the class number, and that's what the model is learning.
+   We can see that the images with the miss classification, the lighter areas correspond to the lighter
+    areas of the chosen class.
 
+2. KNN divides the training set according to the level of similarity to the current sample and makes a decision
+ according to the inputs that are most similar to this image. On the other hand, SVM performs an initial 
+ division according to the distribution of the training set and adjusts the tested input to some of the regions.
+  Therefore, an input that is significantly different from the training set may produce worse results in SVM
+   compared to KNN and on the other hand, inputs from close classes (for example 5 and 6) may be poorly 
+   classified by KNN.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+'''
 
 """
 
 part3_q3 = r"""
-**Your answer:**
+'''
+1. We see a coordinated convergence of the train and the validation and since the train drops a little below the
+ validation but they converge together, and thus this is a good learning rate. If the interval between the train
+  and the validation was larger and we would see a larger decrease in the loss of the train compared to fixation
+   and even an increase in the loss of the validation, we would say that this is over fitting, but this is not 
+   the case. The amount of data in the 5 epochs was enough to get to the convergence point of the model and the
+    following epochs did not contribute significantly to the learning process. Furthermore, to save running time,
+     it is common to add a mechanism that interrupts the learning process if for several epochs the parameters do
+      not change, as in this graph.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. We see that the model is highly underwriting. The model converges, neither in train nor in validation.
+ This corresponds to a too-high learning rate. Another evidence of an excessively high learning rate is the 
+ instability in the validation, which indicates a movement around some local minimum point.
+'''
 
 """
 
